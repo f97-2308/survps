70b4e083034e30ca9d10ac58e02ac3d1cc89d780
f97 <huynhduckhoan@gmail.com>
2022-01-01 11:43:35 +0000

    :boom: add build.sh

---
+++ .github/build.sh
@@ -0,0 +1,17 @@
+git config --global user.email "huynhduckhoan@gmail.com"
+git config --global user.name "f97"
+git clone https://f97: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@github.com/f97/survps.git 
+git clone -b gh-pages  https://f97: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@github.com/f97/survps.git gh-pages
+cp ./survps/README.md ./gh-pages/README.md 
+cd ./gh-pages
+npx github-readme-to-html
+mv dist/index.html .
+rm -rf dist/
+mkdir survps
+cp -rf ../survps/*/ survps
+cp -rf ../survps/centos7 survps
+zip -r sur.zip survps/
+rm -rf survps/
+git commit -a --allow-empty-message -m ':zap: action played'
+git push
+rm -rf ../*/
