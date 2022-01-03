6b866a5403ec6661c3fa2dcde6f23ab143d6563c
f97 <huynhduckhoan@gmail.com>
2022-01-03 03:21:15 +0000

    :boom: rclone with dynamic name

--- update/rclone-cron.sh
+++ update/rclone-cron.sh
@@ -2,7 +2,7 @@
 
 #!/bin/bash
 
-SERVER_NAME=SURVPS_BACKUP
+SERVER_NAME=$(curl -s  ifconfig.me)
 
 TIMESTAMP=$(date +"%F")
 BACKUP_DIR="/root/backup/$TIMESTAMP"
@@ -41,12 +41,12 @@ echo '';
 size=$(du -sh $BACKUP_DIR | awk '{ print $1}')
 
 echo "Starting Uploading Backup";
-rclone move $BACKUP_DIR "s3:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
+rclone move $BACKUP_DIR "remote:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
 # Clean up
 rm -rf $BACKUP_DIR
-rclone -q --min-age 1w delete "s3:$SERVER_NAME" #Remove all backups older than 1 week
-rclone -q --min-age 1w rmdirs "s3:$SERVER_NAME" #Remove all empty folders older than 1 week
-rclone cleanup "s3:" #Cleanup Trash
+rclone -q --min-age 1w delete "remote:$SERVER_NAME" #Remove all backups older than 1 week
+rclone -q --min-age 1w rmdirs "remote:$SERVER_NAME" #Remove all empty folders older than 1 week
+rclone cleanup "remote:" #Cleanup Trash
 echo "Finished";
 echo '';
 
