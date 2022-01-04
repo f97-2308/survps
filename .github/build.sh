e00e1741d780a59462946dc17dc58b81b387b1b6
f97 <35532964+f97@users.noreply.github.com>
2022-01-04 22:36:07 +0700

    Update build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -6,6 +6,7 @@ cd /workspaces/survps/temp
 rm -rf survps gh-pages
 git clone https://f97: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@github.com/f97/survps.git 
 git clone -b gh-pages  https://f97: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@github.com/f97/survps.git gh-pages
+rm -rf ./gh-pages/*
 cp ./survps/README.md ./gh-pages/README.md 
 cp ./survps/install ./gh-pages/install 
 cd ./gh-pages
