18073489f0ccc63913f3a063e07d900d98bd2b0b
f97 <35532964+f97@users.noreply.github.com>
2022-01-08 18:48:09 +0700

    Update build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -4,8 +4,8 @@ git config --global user.email "huynhduckhoan@gmail.com"
 git config --global user.name "f97"
 cd /workspaces/survps/temp
 rm -rf survps gh-pages
-git clone https://f97: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@github.com/f97/survps.git 
-git clone -b gh-pages https://f97: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@github.com/f97/survps.git gh-pages
+git clone https://github.com/f97/survps.git 
+git clone -b gh-pages https://github.com/f97/survps.git gh-pages
 rm -rf ./gh-pages/*
 cp ./survps/README.md ./gh-pages/README.md 
 cp ./survps/install ./gh-pages/install 
